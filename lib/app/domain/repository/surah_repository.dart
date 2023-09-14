f58e1f3a6af2890617b4c6fb7eb98f2c2c489d87
phanatagama <phanatagama@gmail.com>
2023-09-14 11:11:39 +0700

    feat(domain): create surah repository

--- lib/app/domain/repository/surah_repository.dart
+++ lib/app/domain/repository/surah_repository.dart
@@ -1,4 +1,6 @@
 import 'package:dartz/dartz.dart';
+import 'package:suji/app/domain/entities/asmaul_husna.dart';
+import 'package:suji/app/domain/entities/daily_pray.dart';
 import 'package:suji/app/domain/entities/surah.dart';
 import 'package:suji/app/domain/entities/surah_detail.dart';
 import 'package:suji/core/utils/failure.dart';
@@ -8,7 +10,6 @@ abstract class SurahRepository {
   Future<Either<Failure, List<Surah>>> getSurahByQuery(String query);
   Future<Either<Failure, List<SurahDetail>>> getAyahBySurahNumber(
       int surahNumber);
-  Future<Either<Failure, String>> insertOrUpdateSurah(List<Surah> listSurah);
-  Future<Either<Failure, String>> insertOrUpdateAyah(
-      List<SurahDetail> listAyah);
+  Future<Either<Failure, List<DailyPray>>> getDailyPray();
+  Future<Either<Failure, List<AsmaulHusna>>> getAsmaulHusna();
 }
