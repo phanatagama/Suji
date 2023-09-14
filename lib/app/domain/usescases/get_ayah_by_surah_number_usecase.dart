140be58830757b630a3e11ac27dcfd9bb71fb154
phanatagama <phanatagama@gmail.com>
2023-09-14 11:13:38 +0700

    feat(usecase): create get ayah by surah number usecase

---
+++ lib/app/domain/usescases/get_ayah_by_surah_number_usecase.dart
@@ -0,0 +1,13 @@
+import 'package:dartz/dartz.dart';
+import 'package:suji/app/domain/entities/surah_detail.dart';
+import 'package:suji/app/domain/repository/surah_repository.dart';
+import 'package:suji/core/utils/failure.dart';
+
+class GetAyahBySurahNumberUsecase {
+  final SurahRepository surahRepository;
+  GetAyahBySurahNumberUsecase({required this.surahRepository});
+  Future<Either<Failure, List<SurahDetail>>> invoke(
+      int surahNumber) {
+    return surahRepository.getAyahBySurahNumber(surahNumber);
+  }
+}
