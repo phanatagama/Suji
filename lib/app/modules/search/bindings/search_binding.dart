1c9918cc089badd873ceefb022665aae72053667
phanatagama <phanatagama@gmail.com>
2023-09-14 11:18:40 +0700

    feat(ui): create search view

--- lib/app/modules/search/bindings/search_binding.dart
+++ lib/app/modules/search/bindings/search_binding.dart
@@ -1,9 +1,18 @@
 import 'package:get/get.dart';
+import 'package:suji/app/domain/repository/surah_repository.dart';
+import 'package:suji/app/domain/usescases/get_all_surah_usecase.dart';
+import 'package:suji/app/domain/usescases/get_surah_by_query_usecase.dart';
 import 'package:suji/app/modules/search/controllers/search_controller.dart';
 
 class SearchBinding implements Bindings {
   @override
   void dependencies() {
-    Get.lazyPut<MySearchController>(() => MySearchController());
+    Get.lazyPut<MySearchController>(() => MySearchController(
+        getAllSurahUsecase: Get.find<GetAllSurahUsecase>(),
+        getSurahByQueryUsecase: Get.find<GetSurahByQueryUsecase>()));
+    Get.lazyPut(
+        () => GetAllSurahUsecase(surahRepository: Get.find<SurahRepository>()));
+    Get.lazyPut(() =>
+        GetSurahByQueryUsecase(surahRepository: Get.find<SurahRepository>()));
   }
 }
