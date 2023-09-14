50f619409c2f0d69ea89b0b764a48d3d1cacba72
phanatagama <phanatagama@gmail.com>
2023-09-14 12:55:48 +0700

    feat: add app_pages

--- lib/app/routes/app_pages.dart
+++ lib/app/routes/app_pages.dart
@@ -1,9 +1,15 @@
 import 'package:get/get.dart';
+import 'package:suji/app/modules/asmaul_husna/bindings/asmaul_husna_binding.dart';
+import 'package:suji/app/modules/asmaul_husna/views/asmaul_husna_view.dart';
 import 'package:suji/app/modules/lab_ui.dart';
+import 'package:suji/app/modules/menu/bindings/menu_binding.dart';
+import 'package:suji/app/modules/menu/views/menu_view.dart';
 import 'package:suji/app/modules/search/bindings/search_binding.dart';
 import 'package:suji/app/modules/search/views/search_view.dart';
 import 'package:suji/app/modules/shalat/bindings/shalat_binding.dart';
 import 'package:suji/app/modules/shalat/views/shalat_view.dart';
+import 'package:suji/app/modules/tasbih/bindings/tasbih_binding.dart';
+import 'package:suji/app/modules/tasbih/views/tasbih_view.dart';
 
 import '../modules/home/bindings/home_binding.dart';
 import '../modules/home/views/home_view.dart';
@@ -16,28 +22,49 @@ class AppPages {
   AppPages._();
 
   static const INITIAL = Routes.HOME;
+  static const menu = '/menu';
+  static const lab = '/lab';
+  static const shalat = '/shalat';
+  static const search = '/search';
+  static const tasbih = '/tasbih';
+  static const asmaulHusna = '/asmaul-husna';
 
   static final routes = [
-    GetPage(name: "/lab", page: () => const Lab()),
+    GetPage(name: lab, page: () => Lab()),
     GetPage(
       name: _Paths.HOME,
       page: () => const HomeView(),
       binding: HomeBinding(),
     ),
     GetPage(
-      name: "${_Paths.SURAH}/:number",
+      name: '${_Paths.SURAH}/:number',
       page: () => const SurahView(),
       binding: SurahBinding(),
     ),
     GetPage(
-      name: "/search",
+      name: search,
       page: () => const SearchView(),
       binding: SearchBinding(),
     ),
     GetPage(
-      name: "/shalat",
+      name: shalat,
       page: () => ShalatView(),
       binding: ShalatBinding(),
+    ),
+    GetPage(
+      name: menu,
+      page: () => const MenuView(),
+      binding: MenuBinding(),
+    ),
+    GetPage(
+      name: tasbih,
+      page: () => const TasbihView(),
+      binding: TasbihBinding(),
+    ),
+    GetPage(
+      name: asmaulHusna,
+      page: () => const AsmaulHusnaView(),
+      binding: AsmaulHusnaBinding(),
     ),
   ];
 }
