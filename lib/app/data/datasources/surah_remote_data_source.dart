772f83298ed8d0a69c9cb205d85b528582eb5a1e
phanatagama <phanatagama@gmail.com>
2023-09-14 11:07:05 +0700

    feat(data): create surah remote datasource

---
+++ lib/app/data/datasources/surah_remote_data_source.dart
@@ -0,0 +1,52 @@
+import 'package:get/get.dart';
+import 'package:suji/core/utils/error_handler.dart';
+import 'package:suji/core/utils/exception.dart';
+import 'package:suji/app/data/model/surah_detail_response.dart';
+import 'package:suji/core/utils/logger.dart';
+import 'package:suji/core/values/constant.dart';
+
+import '../model/list_surah_response.dart';
+
+abstract class SurahRemoteDataSource {
+  Future<ListSurah> getAllSurah();
+  Future<SurahDetailResponse> getAyahBySurahNumber(int number);
+}
+
+class SurahRemoteDataSourceImpl extends GetConnect
+    implements SurahRemoteDataSource {
+  @override
+  void onInit() {
+    httpClient.baseUrl = ApiConfig.surahBaseURL;
+  }
+
+  @override
+  Future<ListSurah> getAllSurah() async {
+    try {
+      final response = await get(ApiConfig.surahEndpoint);
+      if (response.isOk) {
+        return ListSurah.fromJson(response.body);
+      } else {
+        Log.e('[SurahRemoteDataSource][getAllSurah]', '${response.statusCode}');
+        return errorHandler(response);
+      }
+    } catch (e) {
+        Log.e('[SurahRemoteDataSource][getAllSurah]', e.toString());
+      throw ServerException(e.toString());
+    }
+  }
+
+  @override
+  Future<SurahDetailResponse> getAyahBySurahNumber(int surahNumber) async {
+    try {
+      final response = await get('${ApiConfig.surahEndpoint}$surahNumber');
+      if (response.isOk) {
+        final responseBody = response.body;
+        return SurahDetailResponse.fromJson(responseBody);
+      } else {
+        return errorHandler(response);
+      }
+    } catch (e) {
+      throw ServerException(e.toString());
+    }
+  }
+}
