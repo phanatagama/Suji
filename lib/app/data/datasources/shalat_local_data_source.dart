deef5b82c7dd8c76ba888e69c4967a8b151491a6
phanatagama <phanatagama@gmail.com>
2023-09-14 11:05:54 +0700

    feat(data): create shalat local datasource

---
+++ lib/app/data/datasources/shalat_local_data_source.dart
@@ -0,0 +1,43 @@
+import 'package:geolocator/geolocator.dart';
+import 'package:intl/intl.dart';
+import 'package:suji/app/data/datasources/database_helper.dart';
+import 'package:suji/app/domain/entities/shalat.dart';
+import 'package:suji/core/extensions/string_extension.dart';
+import 'package:suji/core/utils/exception.dart';
+import 'package:suji/core/utils/logger.dart';
+import 'package:suji/core/values/constant.dart';
+
+abstract class ShalatLocalDataSource {
+  Future<Shalat?> getShalatTime(DateTime dateTime, Position position);
+  Future<Shalat?> getShalatTimeByDate(DateTime date);
+  Future<String> insertOrUpdateShalat(List<Shalat> shalat);
+}
+
+class ShalatLocalDataSourceImpl implements ShalatLocalDataSource {
+  final DatabaseHelper databaseHelper;
+  ShalatLocalDataSourceImpl({required this.databaseHelper});
+
+  @override
+  Future<Shalat?> getShalatTime(DateTime dateTime, Position position) async {
+    final date = DateFormat('dd MMM y').format(dateTime);
+    Log.d('[ShalatLocalDataSourceImpl][getShalatTime]', date);
+    final int id = '$date${position.latitude}${position.longitude}'.fastHash();
+    return await databaseHelper.getShalatTime(id);
+  }
+
+  @override
+  Future<String> insertOrUpdateShalat(List<Shalat> shalat) async {
+    try {
+      await databaseHelper.insertOrUpdateShalat(shalat);
+      return AppString.insertOrUpdateSuccess;
+    } catch (e) {
+      throw DatabaseException(e.toString());
+    }
+  }
+
+  @override
+  Future<Shalat?> getShalatTimeByDate(DateTime dateTime) async {
+    final date = DateFormat('dd MMM y').format(dateTime);
+    return await databaseHelper.getShalatTimeByDate(date);
+  }
+}
